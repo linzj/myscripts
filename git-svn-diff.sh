d538f2f87ff191ebac2db85cc9dface2c14d5848
lin <manjian2006@gmail.com>
2014-10-06 21:02:28 +0800

    Add git-svn-diff.sh

---
+++ git-svn-diff.sh
@@ -0,0 +1,22 @@
+#
+# git-svn-diff
+# Generate an SVN-compatible diff against the tip of the tracking branch
+#TRACKING_BRANCH=`git config --get svn-remote.svn.fetch | sed -e 's/.*:refs\/remotes\///'`
+#git diff --no-prefix $(git rev-list --date-order --max-count=1 $TRACKING_BRANCH) $* |
+#sed -e "s/^+++ .*/&    (working copy)/" -e "s/^--- .*/&    (revision $REV)/" \
+#-e "s/^diff --git [^[:space:]]*/Index:/" \
+#-e "s/^index.*/===================================================================/"
+if [ $# -gt 0 ]
+then
+    COMMIT=$1
+else
+    COMMIT=HEAD^
+fi     
+
+echo $COMMIT
+
+REV=`git svn find-rev $COMMIT`
+git diff $COMMIT HEAD --no-prefix |
+sed -e "s/^+++ .*/&    (working copy)/" -e "s/^--- .*/&    (revision $REV)/" \
+-e "s/^diff --git [^[:space:]]*/Index:/" \
+-e "s/^index.*/===================================================================/"
