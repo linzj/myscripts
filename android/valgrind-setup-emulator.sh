edd4784841f62a2a9683f5331bf555a3c9f5472b
linzj <linzj@ucweb.com>
2014-10-21 15:17:37 +0800

    android/ * valgrind-setup-emulator.sh * valgrind-wrapper: valgrind wrapper script for android.

---
+++ android/valgrind-setup-emulator.sh
@@ -0,0 +1,5 @@
+adb shell su -c setprop dalvik.vm.execution-mode int:fast
+adb shell su -c setprop wrap.com.UCMobile /data/local/tmp/wrapper
+adb shell su -c setprop wrap.com.UCMobile:DownloadServi ""
+adb shell su -c setprop wrap.com.UCMobile:RemoteBackgro ""
+#adb shell su -c setprop wrap.com.UCMobile /data/local/tmp/mywrapper.sh
