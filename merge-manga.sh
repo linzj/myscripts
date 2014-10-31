0074bb7bf44197639589011dc8468f44133d80ca
linzj <linzj@ucweb.com>
2014-10-31 18:14:45 +0800

    Fix white space dir problem.

--- merge-manga.sh
+++ merge-manga.sh
@@ -1,26 +1,31 @@
-#!/bin/sh
-DIR=$1
+#!/bin/bash
+shopt -s nocaseglob
+DIR="$1"
+shift
+SUFFIX="$1"
 shift
 
 COUNT=1
 
-DIRS=$#
+DIRS="$#"
+
+IFS=`printf "\n"`
 
 do_move()
 {
-    TARGET=$1
-    for f in `ls $TARGET/*.png`
+    TARGET="$1"
+    for f in $TARGET/*.${SUFFIX}
         do
-            cp $f `printf $DIR/%03d.png ${COUNT}`
+            cp "$f" `printf "$DIR"/%03d.${SUFFIX} ${COUNT}`
             ((COUNT++))
         done
 }
 
 while [ $DIRS -ne 0 ]
     do
-        if [ -d $1 ]
+        if [ -d "$1" ]
             then
-                do_move $1
+                do_move "$1"
         fi
         shift
         ((DIRS--))
