d064fa1ee5aa3d8f254404aaefc9446873481cb0
linzj <linzj@ucweb.com>
2014-11-15 14:38:28 +0800

    * setup-flo-env.sh: Setup kernel build for flo.

---
+++ android/setup-flo-env.sh
@@ -0,0 +1,4 @@
+export ARCH=arm
+export SUBARCH=arm
+export CROSS_COMPILE=arm-linux-androideabi-
+export PATH=/home/linzj/android-ndk-r8e/toolchains/arm-linux-androideabi-4.7/prebuilt/linux-x86_64/bin/:$PATH
