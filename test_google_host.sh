e1ae2a239fa394415ae52db0ec4e9e8b8735e717
linzj <manjian2006@gmail.com>
2014-12-26 23:12:57 +0800

    *test_google_host.sh: A script to test google host. Not finish yet. I will make it up later.

---
+++ test_google_host.sh
@@ -0,0 +1,30 @@
+#74.125.200.1
+test_main ()
+{
+    curl -H 'HOST: www.google.com'  -X GET $1 -w %{http_code} -o /dev/null -s | grep 200 >/dev/null
+}
+
+test_1 ()
+{
+    declare -a my_array
+    COUNT=1
+    OTHER=$1
+    while [ $COUNT -ne 255 ]
+        do
+            test_main $OTHER.$COUNT &
+            my_array[$COUNT]=$!
+            ((++COUNT))
+        done
+    COUNT=1
+    while [ $COUNT -ne 255 ]
+        do
+            wait ${my_array[$COUNT]}
+            if [ $? -eq 0 ]
+                then
+                echo $OTHER.$COUNT is good
+            fi
+            ((++COUNT))
+        done
+}
+
+test_1 74.124.200
