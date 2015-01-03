051188e5641205a22fef8f9dc71b44ab86756243
linzj <manjian2006@gmail.com>
2015-01-03 15:41:01 +0800

    video/ * conv-video.sh: Add double quote to file names.

--- video/conv-video.sh
+++ video/conv-video.sh
@@ -2,9 +2,9 @@
 N=$1
 ON=${1%*.*}.mp4
 COUNT=1
-while [ -e $ON ]
+while [ -e "$ON" ]
 do
     ON=${1%*.*}_$COUNT.mp4
     ((COUNT++))
 done
-avconv $INPUT_OPT -i $N -c:v libx264 -strict experimental -c:a aac $OUTPUT_OPT $ON 
+avconv $INPUT_OPT -i "$N" -c:v libx264 -strict experimental -c:a aac $OUTPUT_OPT "$ON"
