bccedc01dfe94d02aa78a4731eb129875b6aa006
linzj <linzj@ucweb.com>
2015-05-14 16:56:20 +0800

    * xunlei.js: More convenient.

--- xunlei.js
+++ xunlei.js
@@ -1,12 +1,40 @@
 (function () {
+    function DownloadRecord(url) {
+        this.url_ = url;
+        this.name_ = null;
+    }
     var downloads = document.querySelectorAll('input[name*=btdownurl]');
+    var records = []
     for (var i in downloads) {
         var now = downloads[i];
         if (typeof(now.value) == "undefined") {
             break;
         }
-        console.log(now.value);
-        var name = now.previousSibling.previousSibling;
-        console.log(name.value);
+        records.push(new DownloadRecord(now.value));
+    }
+    var names = document.querySelectorAll('a.w_title_open');
+    // ignore the first entry
+    for (var i = 1; i < names.length; ++i) {
+        var name = names[i];
+        var nameString = name.text;
+        records[i - 1].name_ = nameString;
+    }
+    var html = document.documentElement;
+    html.removeAttribute('unselectable');
+    html.style = '-webkit-user-select: yes';
+    var body = document.body
+    myroot = document.createElement('div');
+    body.appendChild(myroot);
+    for (var i in records) {
+        var record = records[i];
+        if (record.name_ == null) {
+            break;
+        }
+        var url = document.createElement('div');
+        url.appendChild(document.createTextNode(record.url_));
+        var name = document.createElement('div');
+        name.appendChild(document.createTextNode(record.name_));
+        myroot.appendChild(url);
+        myroot.appendChild(name);
     }
 })()
