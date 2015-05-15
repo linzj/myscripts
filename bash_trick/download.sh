a9ddabff8d1d997c72df148855333e3f0df65966
linzj <linzj@ucweb.com>
2015-05-15 11:14:11 +0800

    bash_trick/ * download.sh: Make aria2c fail faster

--- bash_trick/download.sh
+++ bash_trick/download.sh
@@ -6,9 +6,10 @@ doDownload ()
 
     #while ! wget -T 6  --limit-rate=1700k --load-cookies cookies.txt -U 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/33.0.1750.152 Safari/537.36' $1 -c -O "$2"
     #while ! curl  -L --limit-rate 300k -c cookies.txt -b cookies.txt -A 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/33.0.1750.152 Safari/537.36' $1 -C - -o "$2"
-    while ! aria2c -x 5 --async-dns-server=8.8.8.8 --load-cookies cookies.txt -U 'Mozilla/5.0 X11 Ubuntu Linux x86_64 rv 15.0 Gecko/20100101 Firefox/15.0' $1 -c --file-allocation=trunc -o $2
+    while ! aria2c -m 0 -t 5 --async-dns-server=8.8.8.8 --load-cookies cookies.txt -U 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/35.0.1916.114 Safari/537.36' $1 -c --file-allocation=trunc -o $2
         do
-            sleep 60
+            sleep 5
+            rm $2
         done
 
 }
@@ -23,5 +24,3 @@ cat downloadList | while read -r site
 
         doDownload  $site $name 
     done
-
-
