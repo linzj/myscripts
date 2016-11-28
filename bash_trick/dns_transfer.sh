c3c80cbf431e49639867b6f04c53df7c7134d542
linzj <zuojian.lzj@alibaba-inc.com>
2016-11-28 13:51:22 +0800

    bash_trick/     * dns_transfer.sh: trap INT to exit.

--- bash_trick/dns_transfer.sh
+++ bash_trick/dns_transfer.sh
@@ -1,8 +1,22 @@
 #!/bin/bash
-iptables -t nat -A OUTPUT -d 10.1.12.31/24 -j DNAT --to-destination 8.8.8.8
+
+function ctrl_c() {
+    do_exit
+}
+
+function do_exit() {
+    echo 'exiting'
+    iptables -t nat -D OUTPUT 1
+    exit 0
+}
+
+iptables -t nat -A OUTPUT -d 100.84.12.31/24 -j DNAT --to-destination 8.8.8.8
+#iptables -t nat -A OUTPUT -d 192.168.1.1 -j DNAT --to-destination 8.8.8.8
 #iptables -t nat -A OUTPUT -p tcp --dport 443 -j REDIRECT --to-port 80
 iptables -L -t nat
+trap ctrl_c INT
+
 read
-iptables -t nat -D OUTPUT 1
+do_exit
 #iptables -t nat -D OUTPUT 1
 
