7fe706f9fa00ea678ea07fd31644bed71c0cd32b
linzj <zuojian.lzj@alibaba-inc.com>
2017-06-15 09:42:00 +0800

    buildscript/     llvm/         * config_withoutrt.sh: Config LLVM without compiler-rt for         android.

---
+++ buildscript/llvm/config_withoutrt.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+BUILDTYPE=Release
+if [ $# -ne 0 ]; then
+    if [ $1 = "Debug" ]; then
+        BUILDTYPE=Debug
+    fi
+fi
+
+CC=clang CXX=clang++ cmake ../ -DCMAKE_BUILD_TYPE=$BUILDTYPE -DLLVM_TARGETS_TO_BUILD="X86;ARM;AArch64" -DLLVM_BINUTILS_INCDIR=/usr/include/
