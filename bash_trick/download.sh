63cfe6aea6170bf2554730dfa1372905c229883f
linzj <zuojian.lzj@alibaba-inc.com>
2017-09-27 10:02:46 +0800

    bash_trick/     * download.sh: make aria2c is able to download with multiple connections.

--- bash_trick/download.sh
+++ bash_trick/download.sh
@@ -6,7 +6,8 @@ doDownload ()
 
     #while ! wget -T 6  --limit-rate=1700k --load-cookies cookies.txt -U 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/33.0.1750.152 Safari/537.36' $1 -c -O "$2"
     #while ! curl  -L --limit-rate 300k -c cookies.txt -b cookies.txt -A 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/33.0.1750.152 Safari/537.36' $1 -C - -o "$2"
-    while ! aria2c -m 0 -t 5 --async-dns-server=8.8.8.8 --load-cookies cookies.txt -U 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/35.0.1916.114 Safari/537.36' $1 -c --file-allocation=trunc -o $2
+    #while ! aria2c -m 0 -t 5 --async-dns-server=8.8.8.8 --load-cookies cookies.txt -U 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/35.0.1916.114 Safari/537.36' $1 -c --file-allocation=trunc -o $2
+    while ! aria2c -m 0 -t 5 -s 10 -x 5 -k 1048576 --async-dns-server=8.8.8.8 --load-cookies cookies.txt -U 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/35.0.1916.114 Safari/537.36' $1 -c --file-allocation=trunc -o $2
         do
             sleep 5
             rm $2
