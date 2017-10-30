7a637389e14bf4c839b952882492ae668cf1a846
linzj <zuojian.lzj@alibaba-inc.com>
2017-10-30 17:50:41 +0800

    buildscript/     * build_nromalgcc.sh: Build normal GCC, without additional test.

---
+++ buildscript/build_normalgcc.sh
@@ -0,0 +1,1 @@
+CFLAGS="-g -O0" CXXFLAGS="-g -O0" $1 --disable-bootstrap --disable-nls --disable-libitm --disable-libcilkrts --disable-sjlj-exceptions  --enable-languages=c,c++ --disable-libvtv --disable-libatomic 
