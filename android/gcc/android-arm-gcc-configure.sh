804ef789899df0c3f12f25de093fbc15e63b848f
linzj <zuojian.lzj@alibaba-inc.com>
2017-11-24 14:10:30 +0800

    android/gcc     * android-arm-gcc-configure.sh: Separate the binutils build and gcc     build.

--- android/gcc/android-arm-gcc-configure.sh
+++ android/gcc/android-arm-gcc-configure.sh
@@ -28,5 +28,40 @@ else
     build_static_libs
 fi
 export CFLAGS=-fPIC
+# setup target tools
+
+NDK_ROOT=`dirname $(which ndk-build)`
+if [ -z $NDK_ROOT ]; then
+  echo failed to detect ndk root
+  exit 1
+fi
+TARGET_ARCH=armeabi
+export ANDROID_NDK_ROOT=$NDK_ROOT
+
+# If not defined ANDROID_TOOLCHAIN, it search the ndk directory and find the gcc-4.4.3 to compile v8.
+if [ -z "$ANDROID_TOOLCHAIN" ]; then
+    if [ "$TARGET_ARCH" = "x86" ]; then
+        TOOLCHAIN_PATH=`dirname $(find $NDK_ROOT/toolchains $NDK_ROOT/build  -name 'i686*' -name '*-g++' | sort | head -n 1)`
+    elif [ "$TARGET_ARCH" = "armeabi" ]; then
+        TOOLCHAIN_PATH=`dirname $(find $NDK_ROOT/toolchains $NDK_ROOT/build  -name 'arm*' -name '*-g++' | sort | head -n 1)`
+        EXTRA_OPTION="armv7=false vfp2=off vfp3=off"
+    elif [ "$TARGET_ARCH" = "armeabi-v7a" ]; then
+        TOOLCHAIN_PATH=`dirname $(find $NDK_ROOT/toolchains $NDK_ROOT/build  -name 'arm*' -name '*-g++' | sort | head -n 1)`
+        EXTRA_OPTION="armv7=true vfp2=on vfp3=on"
+    else
+        echo "unsupported platform " "$TARGET_ARCH" 1>&2
+        exit 1
+    fi
+    export ANDROID_TOOLCHAIN=${TOOLCHAIN_PATH%/bin}
+fi
+
+export AR_FOR_TARGET=$ANDROID_TOOLCHAIN/arm-linux-androideabi/bin/ar
+export AS_FOR_TARGET=$ANDROID_TOOLCHAIN/arm-linux-androideabi/bin/as
+export LD_FOR_TARGET=$ANDROID_TOOLCHAIN/arm-linux-androideabi/bin/ld.bfd
+export NM_FOR_TARGET=$ANDROID_TOOLCHAIN/arm-linux-androideabi/bin/nm
+export OBJCOPY_FOR_TARGET=$ANDROID_TOOLCHAIN/arm-linux-androideabi/bin/objcopy
+export OBJDUMP_FOR_TARGET=$ANDROID_TOOLCHAIN/arm-linux-androideabi/bin/objdump
+export RANLIB_FOR_TARGET=$ANDROID_TOOLCHAIN/arm-linux-androideabi/bin/ranlib
+export READELF_FOR_TARGET=$ANDROID_TOOLCHAIN/arm-linux-androideabi/bin/readelf
 
 $1 --prefix=`pwd`/built --target=arm-linux-androideabi --with-gnu-as  --enable-languages=c,c++ --disable-libssp --disable-nls --disable-libitm --disable-libmudflap --enable-plugins --enable-libgomp --enable-graphite=yes --enable-libstdc__-v3 --disable-sjlj-exceptions --disable-tls --enable-gold --with-float=soft --with-fpu=vfp --with-arch=armv5te --enable-target-optspace --enable-initfini-array --disable-nls --with-sysroot=/home/linzj/android-ndk-r10e/platforms/android-21/arch-arm --with-arch=armv5te --program-transform-name='s,^,arm-linux-androideabi-,' --enable-shared=libsanitizer --with-gmp-lib=$STATIC_LIBS --with-mpc-lib=$STATIC_LIBS --with-mpfr-lib=$STATIC_LIBS --with-isl-lib=$STATIC_LIBS
