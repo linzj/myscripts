8628e44d671066a23f7751bf0331800b9948d4a4
linzj <zuojian.lzj@alibaba-inc.com>
2018-12-10 08:53:32 +0800

    android/     * invoke-gdbserver-usr-push.sh: Use push to invoke gdbserver.

---
+++ android/invoke-gdbserver-usr-push.sh
@@ -0,0 +1,26 @@
+PROCESS_NAME=com.UCMobile
+if [ $# -gt 0 ]
+then
+PROCESS_NAME=$1
+fi
+IFS=':' read -r -a array <<< $PROCESS_NAME
+PACKAGE_NAME=${array[0]}
+
+MYASS=`adb shell ps | tr "\r\n" "\n" | grep -Pwm1 "$PROCESS_NAME"'$' | awk '{print $2}'`
+if [ -z $MYASS ]; then
+  MYASS=`adb shell ps -A | tr "\r\n" "\n" | grep -Pwm1 "$PROCESS_NAME"'$' | awk '{print $2}'`
+fi
+
+if [ -z $MYASS ]; then
+  MYASS=$2
+fi
+echo $MYASS
+DEBUG_SOCKET=debug-socket
+DEBUG_PORT=5039
+DATA_DIR=/data/data/$PACKAGE_NAME
+adb forward tcp:$DEBUG_PORT localfilesystem:$DATA_DIR/$DEBUG_SOCKET
+adb push ~/android-ndk-r10e/prebuilt/android-arm/gdbserver/gdbserver /sdcard/
+adb shell run-as $PACKAGE_NAME cp /sdcard/gdbserver /data/data/$PACKAGE_NAME/gdbserver
+adb shell run-as $PACKAGE_NAME chmod 755 /data/data/$PACKAGE_NAME/gdbserver
+adb shell run-as $PACKAGE_NAME /data/data/$PACKAGE_NAME/gdbserver +$DEBUG_SOCKET  --attach $MYASS
+
