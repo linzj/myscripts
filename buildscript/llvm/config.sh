b3306de90662d2e94f09c5349ecb48888488ce93
linzj <zuojian.lzj@alibaba-inc.com>
2019-05-28 10:47:37 +0800

    buildscript/llvm/     * config.sh: Build default Release or Debug clang/llvm.

--- buildscript/llvm/config.sh
+++ buildscript/llvm/config.sh
@@ -1,15 +1,10 @@
-# Use the build standalone toolchian to get the toolchain first.
-ANDROID_TOOLCHAIN=/home/linzj/src/llvm/building/build-rtx86/android-toolchain
-ANDROID_BUILD_DIR=.
-LLVM_CHECKOUT=../../
-BUILD_TYPE=Release
+#!/bin/sh
 
-# Always clobber android build tree.
-# It has a hidden dependency on clang (through CXX) which is not known to
-# the build system.
-mkdir $ANDROID_BUILD_DIR
-(cd $ANDROID_BUILD_DIR && \
- cmake -DCMAKE_BUILD_TYPE=$BUILD_TYPE \
- -DLLVM_ANDROID_TOOLCHAIN_DIR=$ANDROID_TOOLCHAIN \
- -DCMAKE_TOOLCHAIN_FILE=$LLVM_CHECKOUT/cmake/platforms/Android_x86.cmake \
- $LLVM_CHECKOUT)
+BUILDTYPE=Release
+if [ $# -ne 0 ]; then
+    if [ $1 = "Debug" ]; then
+        BUILDTYPE=Debug
+    fi
+fi
+
+CC=clang CXX=clang++ cmake ../ -GNinja -DCMAKE_BUILD_TYPE=$BUILDTYPE -DLLVM_ENABLE_LIBEDIT=OFF -DLLVM_ENABLE_LIBXML2=OFF -DLLVM_ENABLE_TERMINFO=OFF -DLLVM_TARGETS_TO_BUILD="X86;ARM;AArch64" -DCMAKE_EXE_LINKER_FLAGS="-static-libgcc -static-libstdc++" -DCMAKE_SHARED_LINKER_FLAGS="-static-libgcc -static-libstdc++" -DLLVM_BINUTILS_INCDIR=/usr/include/
