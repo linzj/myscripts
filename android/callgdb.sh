e4641bd793d505ba457f487bc469484ff2cf5e1f
linzj <zuojian.lzj@alibaba-inc.com>
2019-11-06 15:27:00 +0800

    android/     * callgdb.sh: New chromium supported.

--- android/callgdb.sh
+++ android/callgdb.sh
@@ -1,5 +1,9 @@
+cd src/out/Android_Debug/
 mkdir -p rom-tmp/
 rm rom-tmp/*
 adb pull /system/lib/libc.so rom-tmp/libc.so
 adb pull /system/bin/app_process32 rom-tmp/app_process32
-~/src/binutils-gdb/building/built/usr/local/bin/arm-linux-androideabi-gdb rom-tmp/app_process32 -ex 'set solib-search-path rom-tmp:src/out/Release/lib.unstripped/' -ex 'set sysroot /' -ex 'target remote :5039'
+mkdir -p so-tmp/
+mv *.so so-tmp/
+~/src/binutils-gdb/building/built/usr/local/bin/arm-linux-androideabi-gdb rom-tmp/app_process32  -ex 'set solib-search-path rom-tmp:lib.unstripped' -ex 'set sysroot /' -ex 'target remote :5039'
+mv so-tmp/* .
