15554144381223879011f5204543bc157b8950e6
linzj <zuojian.lzj@alibaba-inc.com>
2020-11-12 09:32:33 +0800

    android/     * setup_android_backup.sh: Setup android backup that compatible to     the network environment in China.

---
+++ android/setup_android_backup.sh
@@ -0,0 +1,4 @@
+adb shell settings put global captive_portal_http_url http://www.google.cn/generate_204
+adb shell settings put global captive_portal_https_url https://www.google.cn/generate_204
+adb shell settings put global captive_portal_fallback_url http://www.google.cn/generate_204
+adb shell settings put global captive_portal_other_fallback_urls http://www.qualcomm.cn/generate_204
